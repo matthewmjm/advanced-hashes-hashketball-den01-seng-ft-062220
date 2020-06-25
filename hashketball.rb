528f38db0c302a80ccfb32f07c06daf593b2c28a
matthewmjm <matthewmjm@yahoo.com>
2020-06-25 12:43:15 -0600

    completed lab

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 #game_hash
 def game_hash
     {
@@ -125,133 +126,80 @@ def game_hash
       }
     }
   end
-  t
+  
+
+  #helper method - combine home and away players
+  def both_teams
+    return all_players = game_hash[:home][:players] + game_hash[:away][:players]
+  end
   
   #num_points_scored  
-  def num_points_scored(player_name)
-    points_scored = 0
-    home_players = game_hash[:home][:players]
-    away_players = game_hash[:away][:players]
-    all_players = (away_players + home_players)
-    all_players.each do |element|
-      element.each do |key, value|
-        if element[key] == player_name
-          points_scored = element[:points]
-          return points_scored
-        end
-      end
+  def num_points_scored(name)
+    both_teams.each do |player|
+      return player[:points] if player[:player_name] == name
     end
-  end 
-  
-  
-  #shoe_size
-  def shoe_size(player_name)
-    shoe_size = 0
-    home_players = game_hash[:home][:players]
-    away_players = game_hash[:away][:players]
-    all_players = (away_players + home_players)
-    all_players.each do |element|
-      element.each do |key, value|
-        if element[key] == player_name
-          shoe_size = element[:shoe]
-          return shoe_size
-        end
-      end
+  end
+
+  #num_points_scored  
+  def shoe_size(name)
+    both_teams.each do |player|
+      return player[:shoe] if player[:player_name] == name
     end
-  end 
-  
-  
+  end
+
   #team_colors
-  def team_colors(tc_team_name)
-    result_team_colors = Array.new
-    game_hash.each do |key, value|
-      value.each do |key2, value2|
-        if value2 == tc_team_name
-          result_team_colors = game_hash[key][:colors]
-          return result_team_colors
-        end
-      end
-    end
-  end
-  
-  
-  #team_names
-  def team_names()
-    team_array = []
-    game_hash.each do |key, value|
-      value.each do |key2, value2|
-        if key2 == :team_name
-          team_array.push(game_hash[key][key2])
-        end 
-      end 
+  def team_colors(team)
+    if team == game_hash[:away][:team_name]
+      return game_hash[:away][:colors]
+    else
+      return game_hash[:home][:colors] 
     end 
-    return team_array
   end
-  
-  
-  #player_numbers
-  def player_numbers(tm_name)
-    jersey_array = []
-    if game_hash[:home][:team_name] == tm_name
-      game_hash[:home][:players].each do |element|
-        element.each do |key3, value3|
-          if key3 == :number
-            jersey_array.push(value3)
-          else
-          end
-        end
+
+  #team_names
+  def team_names
+    teams = []
+    teams << game_hash[:home][:team_name] << game_hash[:away][:team_name]
+  end
+
+ #player_numbers
+  def player_numbers(team)
+    jerseys = []
+    if game_hash[:away][:team_name] == team
+      game_hash[:away][:players].map do |player|
+        jerseys = jerseys << player[:number]
       end
     else
-      game_hash[:away][:team_name] == tm_name
-      game_hash[:away][:players].each do |element|
-        element.each do |key3, value3|
-          if key3 == :number
-            jersey_array.push(value3)
-          else
-          end
-        end
+      game_hash[:home][:players].each do |player|
+        jerseys = jerseys << player[:number]
       end
-    end
-    jersey_array
+    end 
+    jerseys
   end
-    
-  
-  #player_stats
-  def player_stats(player_name)
-    player_statistics = {}
-    home_players = game_hash[:home][:players]
-    away_players = game_hash[:away][:players]
-    all_players = (away_players + home_players)
-    all_players.each do |element|
-      element.each do |key, value|
-        if element[key] == player_name
-          player_statistics = element
-        return player_statistics
-        end
-      end
-    end
+
+  ##player_stats
+  def player_stats(name)
+    both_teams.each do |player|
+      return player if player[:player_name] == name
+    end 
   end
-  
-  
+
+  ##player_stats
+  def big_shoe_rebounds(name)
+    both_teams.each do |player|
+      return player if player[:player_name] == name
+    end 
+  end
+
   #big_shoe_rebounds
   def big_shoe_rebounds
     largest_shoe = 0
-    result = {}
-    home_players = game_hash[:home][:players]
-    away_players = game_hash[:away][:players]
-    all_players = (away_players + home_players)
-    all_players.each do |element|
-      element.each do |key, value|
-        if key == :shoe
-          if value > largest_shoe
-            largest_shoe = value
-            result = element
-          else
-          end
-        else
-        end
+    result_rebounds = 0
+    both_teams.each do |player|
+      if player[:shoe] > largest_shoe
+        largest_shoe = player[:shoe]
+        result_rebounds = player[:rebounds]
       end
-    end
-    return result[:rebounds]
+    end 
+    result_rebounds
   end
-  
